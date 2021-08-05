469de66b21a76adc88e763ce3f973381b46d45d7
ivanhsiav <vaishnavi1eranti@gmail.com>
2021-08-04 19:54:30 -0700

    still test commit

--- update_website.sh
+++ update_website.sh
@@ -1,3 +1,12 @@
+#Hello there.
+#this file will automatically update the website on github
+#Steps to do this:
+#1. Enter the terminal(usually see it below)
+#2. type ./update_website.sh and then tell what changes you made in quotes
+#   For example, i will type:
+#   ./update_website.sh "added illustrations to story"
+#3. You will then see a popup and come code running. Wait one second...
+
 echo "starting process?";
 git status;
 echo "adding files"
@@ -6,5 +15,7 @@ echo "added necessary files to git system";
 git commit -m "$1";
 echo "successfully committed $1 to github";
 git push origin main;
+
 echo "congrats! commit '$1' was pushed to github. Go to github and view updates";
 
+sleep 2;
